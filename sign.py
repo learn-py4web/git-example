4c21ae5597a733e1378fa6cc57869cf310585ee8
Luca de Alfaro <luca@dealfaro.org>
2022-05-06 14:47:46 -0700

    Second commit

--- sign.py
+++ sign.py
@@ -3,8 +3,8 @@ import hashlib
 from secrets import signing_key
 
 print("Welcome to my superb signing program")
-s = input("String to encode: ")
+s = input("String to sign: ")
 h = hashlib.sha256(signing_key.encode('utf-8'))
 h.update(s.encode('utf-8'))
-print("Signed: {}".format(h.hexdigest()))
+print("Signature: {}".format(h.hexdigest()))
 
